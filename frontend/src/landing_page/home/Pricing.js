bcece32191c7701d7f9d089167ae0f0ccd5bce7e
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-13 12:39:41 +0530

    added home -> Pricing

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -2,11 +2,26 @@ import React from 'react';
 
 function Pricing() {
     return (
-        <div className='container'>
-            <div className='row'>
-                <div className='col-4'></div>
+        <div className='container p-3'>
+            <div className='row mx-5'>
+                <div className='col-4 '>
+                    <h1 className='mb-3'>Unbeatable pricing</h1>
+                    <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                    <a href='' className='' style={{textDecoration: "none"}}>See pricing <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
                 <div className='col-2'></div>
-                <div className='col-6'></div>
+                <div className='col-6'>
+                    <div className='row text-center'>
+                        <div className='col-6 border p-4'>
+                            <h1 className='mb-4'>₹0</h1>
+                            <p>Free equity delivery and<br/> direct mutual funds</p>
+                        </div>
+                        <div className='col-6 border p-4'>
+                            <h1 className='mb-4'>₹20</h1>
+                            <p>Intraday and F&O</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
