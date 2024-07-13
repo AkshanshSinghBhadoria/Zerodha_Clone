933c2e2e2ffcd8827b923861a3fd2c1210007d75
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-13 18:27:02 +0530

    added openacc.

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function OpenAccount() {
     return (
-        <h1>Open Account</h1>
+        <div className='container p-5 mb-5'>
+        <div className='row text-center'>
+            <h1 className='mt-5'>Open a Zerodha account</h1>
+            <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+            <button className='p-2 fs-5 btn btn-primary mb-5' style={{width: "15%", margin: "0 auto"}}>Sign Up Now</button>
+        </div>
+      </div>
     );
 }
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
             <img src='media/images/homeHero.png' alt='hero img' className='mb-5' ></img>
             <h1 className='mt-5'>Invest in everything</h1>
             <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-            <button className='p-2 fs-5 btn btn-primary mb-5' style={{width: "20%", margin: "0 auto"}}>Sign Up Now</button>
+            <button className='p-2 fs-5 btn btn-primary mb-5' style={{width: "15%", margin: "0 auto"}}>Sign Up Now</button>
         </div>
       </div>
     );
