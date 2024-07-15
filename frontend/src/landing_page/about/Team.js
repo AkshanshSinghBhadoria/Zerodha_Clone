5e6f392f5ae3c2c365a8e066e21251acd18c88e6
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-15 13:42:42 +0530

    added about

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
 
 function Team() {
-    return (
-        <h1>Team</h1>
-    );
+  return (
+    <div className="container">
+      <div className="row p-5 border-top">
+        <h1 className=" text-center mt-5">People</h1>
+      </div>
+      <div
+        className="row  text-muted"
+        style={{ width: "1.8", fontSize: "1.2em" }}
+      >
+        <div className="col-6 p-5 text-center">
+          <img
+            src="media\images\nithinKamath.jpg"
+            style={{ borderRadius: "100%", width: "50%" }} 
+          ></img>
+          <h4 className="mt-4">Nithin Kamath</h4>
+          <h6>Founder, CEO</h6>
+        </div>
+        <div className="col-6 p-5" style={{ fontSize: "0.9em" }}>
+          <p>
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
+            hurdles he faced during his decade long stint as a trader. Today,
+            Zerodha has changed the landscape of the Indian broking industry.
+          </p>
+          <p>
+            He is a member of the SEBI Secondary Market Advisory Committee
+            (SMAC) and the Market Data Advisory Committee (MDAC).
+          </p>
+          <p>Playing basketball is his zen.</p>
+          <p>
+            Connect on <a href="">Homepage</a> / <a href="">TradingQnA</a> /
+            <a href="">Twitter</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Team;
