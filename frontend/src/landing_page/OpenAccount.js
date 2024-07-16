7b987fb633d0ef5a2624e2bbe23ee68d8a29a0b1
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-16 21:08:23 +0530

    added open acc to pricing

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -5,7 +5,7 @@ function OpenAccount() {
         <div className='container p-5 mb-5'>
         <div className='row text-center'>
             <h1 className='mt-5'>Open a Zerodha account</h1>
-            <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+            <p className='my-4'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
             <button className='p-2 fs-5 btn btn-primary mb-5' style={{width: "15%", margin: "0 auto"}}>Sign Up Now</button>
         </div>
       </div>
--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
 import Brokerage from './Brokerage';
+import OpenAccount from '../OpenAccount';
 
 function PricingPage() {
     return (
         <>
           <Hero/>
           <Brokerage/>
+          <OpenAccount/>
         </>
     );
 }
