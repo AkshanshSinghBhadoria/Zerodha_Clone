ee1a62c71d118b66c827e175e7abdc4a49ab8dcc
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-17 01:55:14 +0530

    added pricing

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
 
 function Brokerage() {
-    return (
-        <h1>Brokerage</h1>
-    );
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 border-top">
+        <div className="col-8 p-5">
+          <a href="" style={{ textDecoration: "none" }}>
+            <p className="fs-5 mb-4" style={{ marginLeft:"40px" }}>Brokerage calculator</p>
+          </a>
+          <ul className="text-muted " style={{ fontSize:"14px", lineHeight:"2" }}>
+            <li>
+              Call & Trade and RMS auto-squareoff: Additional charges of ₹50 +
+              GST per order.
+            </li>
+            <li>Digital contract notes will be sent via e-mail.</li>
+            <li>
+              Physical copies of contract notes, if required, shall be charged
+              ₹20 per contract note. Courier charges apply.
+            </li>
+            <li>
+              For NRI account (non-PIS), 0.5% or ₹100 per executed order for
+              equity (whichever is lower).
+            </li>
+            <li>
+              For NRI account (PIS), 0.5% or ₹200 per executed order for equity
+              (whichever is lower).
+            </li>
+            <li>
+              If the account is in debit balance, any order placed will be
+              charged ₹40 per executed order instead of ₹20 per executed order.
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 p-5">
+          <a href="" style={{ textDecoration: "none" }}>
+            <p className="fs-5">List of charges</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Brokerage;
