1b23dd2b4becb7a4d839cb2bfde47536862a8bd7
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-17 21:16:51 +0530

    added support

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return (
-        <h1>Hero</h1>
-    );
+  return (
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5" id="supportWrapper">
+        <h4 className="">Support Portal</h4>
+        <a href="" style={{ color: "white" }}>Track tickets</a>
+      </div>
+      <div className="row p-5 mx-5">
+      <div className="col-6 mb-5">
+        <h1 className="fs-4">Search for an answer or browse help topics to create a ticket</h1>
+        <input placeholder="Eg. How do i activate F&O, why is my order getting rejected..." /> <br/><br/>
+        <a href="" style={{ color: "white",fontSize:"18px",marginLeft:"1rem" }}>Track account opening </a>
+        <a href="" style={{ color: "white",fontSize:"18px",marginLeft:"1rem" }}>Track segment activation </a>
+        <a href="" style={{ color: "white",fontSize:"18px",marginLeft:"1rem" }}>Intraday margins </a> <br/>
+        <a href="" style={{ color: "white",fontSize:"18px",marginLeft:"1rem" }}>Kite user manual</a>
+      </div>
+      <div className="col-6">
+        <h1 className="fs-4" style={{marginLeft:"20px"}}>Featured</h1>
+        <ol style={{marginLeft:"20px"}}>
+            <li style={{marginBottom:"20px"}}><a href="" style={{ color: "white"}}>Surveillance measure on scrips - July 2024 </a></li>
+            <li><a href="" style={{ color: "white"}}>Latest Intraday leverages and Square-off timings </a></li>
+        </ol>
+      </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
