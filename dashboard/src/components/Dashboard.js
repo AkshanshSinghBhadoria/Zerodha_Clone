e22f0e9fbac1bd8845e38d54b1e4063aaf39cc35
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-18 01:49:31 +0530

    fixing errors

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -9,14 +9,11 @@ import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
-import { GeneralContextProvider } from "./GeneralContext";
 
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
-      <GeneralContextProvider>
         <WatchList />
-      </GeneralContextProvider>
       <div className="content">
         <Routes>
           <Route exact path="/" element={<Summary />} />
