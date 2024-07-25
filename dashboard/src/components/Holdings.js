e27e43182eb97f02369a65eab22b290a2bb15b03
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-07-26 00:34:09 +0530

    Holdings.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { holdings } from "../data/data";
 
 const Holdings = () => {
   return (
     <>
-      <h3 className="title">Holdings</h3>
+      <h3 className="title">Holdings ({ holdings.length })</h3>
 
       <div className="order-table">
         <table>
@@ -17,6 +18,26 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
+
+          {holdings.map((stock, index) => {
+            const curValue = stock.price * stock.qty;
+            const isProfit = curValue - stock.avg * stock.qty >= 0.0;
+            const profClass = isProfit ? "profit" : "loss";
+            const dayClass = stock.isloss ? "loss" : "profit";
+
+            return (
+              <tr key={index} >
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{curValue.toFixed(2)}</td>
+                <td className={profClass}>{(curValue - stock.avg * stock.qty).toFixed(2)}</td>
+                <td className={profClass}>{stock.net}</td>
+                <td className={dayClass}>{stock.day}</td>
+              </tr>
+            );
+          })}
         </table>
       </div>
 
