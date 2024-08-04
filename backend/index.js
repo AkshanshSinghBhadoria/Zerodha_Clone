5b863c39adbcbc36f38243cacb433e39a9f596b9
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-08-04 10:30:04 +0530

    Connected to mongo

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const url = process.env.MONGO_URL;
 
 const app = express();
 
-app.listen(3002, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on port 3002");
+    mongoose.connect(url);
+    console.log("DB connected");
 });
