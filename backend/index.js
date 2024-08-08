829feb8682d3f746eaef5a486e5518ada45bd5c4
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-08-08 20:21:18 +0530

    adding temp data

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
 
 const PORT = process.env.PORT || 3002;
 const url = process.env.MONGO_URL;
@@ -134,6 +135,47 @@ const app = express();
 //         });
 
 //         newHolding.save();
+//     });
+//     res.send("Done!");
+// });
+
+// app.get("/addPositions", async(req, res) => {
+//     let tempPositions = [
+//         {
+//           product: "CNC",
+//           name: "EVEREADY",
+//           qty: 2,
+//           avg: 316.27,
+//           price: 312.35,
+//           net: "+0.58%",
+//           day: "-1.24%",
+//           isLoss: true,
+//         },
+//         {
+//           product: "CNC",
+//           name: "JUBLFOOD",
+//           qty: 1,
+//           avg: 3124.75,
+//           price: 3082.65,
+//           net: "+10.04%",
+//           day: "-1.35%",
+//           isLoss: true,
+//         },
+//       ];
+
+//       tempPositions.forEach((item) => {
+//         let newPosition = new PositionsModel({
+//             product: item.product,
+//             name: item.name,
+//             qty: item.qty,
+//             avg: item.avg,
+//             price: item.price,
+//             net: item.net,
+//             day: item.day,
+//             isLoss: item.isLoss,
+//         });
+
+//         newPosition.save();
 //     });
 //     res.send("Done!");
 // });
